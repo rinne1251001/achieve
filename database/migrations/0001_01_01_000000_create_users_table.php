741dd7834843256625daa3f93c86cb2f2056e922
ta9tstock <goncotaro@gmail.com>
2026-01-29 10:04:43 +0900

    users　point追加

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('theme_color')->default('aqua');
+            $table->integer('point')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
