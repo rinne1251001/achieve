d58f57a18c58f9861c6877e5262bc311078d8628
rinne1251001 <cfrm1251001@gn.iwasaki.ac.jp>
2026-02-03 12:05:05 +0900

    メール

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,29 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // パスワードリセットメールの文面を完全に書き換える
+        ResetPassword::toMailUsing(function ($notifiable, $token) {
+            $resetUrl = url(config('app.url').route('password.reset', [
+                'token' => $token,
+                'email' => $notifiable->getEmailForPasswordReset(),
+            ], false));
+
+            return (new MailMessage)
+                ->subject('【' . config('app.name') . '】パスワード再設定のお知らせ')
+                ->greeting('こんにちは。')
+                ->line('タスク提案アプリ「' . config('app.name') . ' 」にて、')
+                ->line('パスワード再設定のリクエストが行われました。')
+                ->line('下記のボタンから、新しいパスワードを設定してください。')
+                ->line('このリンクの有効期限は 60分 です。')
+                ->action('パスワードをリセットする', $resetUrl)
+                ->line('本手続きにお心当たりがない場合、')
+                ->line('第三者による操作の可能性があります。')
+                ->line('その場合は、本メールを破棄し、再設定操作は行わないでください。')
+                ->line('安全のため、定期的なパスワード変更を推奨いたします。')
+                ->salutation("――――――――\n" . 
+                 config('app.name') . "\n" . 
+                 "Email：noreply@achieve-on-step.sakuraweb.com\n" . 
+                 "URL：https://achieve-on-step.sakuraweb.com");
+        });
     }
 }
---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+@props(['url'])
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+    <img src="https://achieve-on-step.sakuraweb.com/images/favicon.ico" class="logo" alt="achieve on step Logo">
+</a>
+</td>
+</tr>
